d492616965ecb94cdf8f820ffd3af1d3c5632603
Andre Girao <andrelgirao22@hotmail.com>
2018-08-21 19:57:32 -0300

    criado pagina se Signup

---
+++ src/app/model/accountDTO.ts
@@ -0,0 +1,16 @@
+
+export interface AccountDTO {
+    
+    email: string
+	password:string
+	addressName:string 
+	addressNumber:string
+	neighborhood:string
+	cityId:string
+	complement?:string
+	register:string
+    postalCode:string
+    imageUrl?: string
+
+
+}
